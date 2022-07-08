702d0a18fe8720d39c5af11d2d9d733087c2c1a4
tupercav <tupercav@gmail.com>
2022-07-08 11:34:22 +0300

    11

---
+++ src/main/java/ru/tupercav/springMVC/HelloController.java
@@ -0,0 +1,14 @@
+package ru.tupercav.springMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloWorldPage() {
+        return "hello-world";
+    }
+}
